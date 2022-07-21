75cf9a0a2cab6cce59a07364b370a72bd4cba1ea
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-07-21 15:46:19 -0700

    macros

--- macros/get_search_term_report_columns.sql
+++ macros/get_search_term_report_columns.sql
@@ -2,12 +2,14 @@
 
 {% set columns = [
     {"name": "_fivetran_id", "datatype": dbt_utils.type_string()},
+    {"name": "ad_group_deleted", "datatype": "boolean"},
     {"name": "ad_group_id", "datatype": dbt_utils.type_int()},
     {"name": "ad_group_name", "datatype": dbt_utils.type_string()},
     {"name": "bid_amount_amount", "datatype": dbt_utils.type_numeric()},
     {"name": "bid_amount_currency", "datatype": dbt_utils.type_string()},
     {"name": "campaign_id", "datatype": dbt_utils.type_int()},
     {"name": "date", "datatype": "date"},
+    {"name": "deleted", "datatype": "boolean"},
     {"name": "impressions", "datatype": dbt_utils.type_int()},
     {"name": "keyword", "datatype": dbt_utils.type_string()},
     {"name": "keyword_display_status", "datatype": dbt_utils.type_string()},
