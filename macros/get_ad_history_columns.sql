aaa93bebd46818e8c70d0ddc239697e45bf7a4e6
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-09-06 13:38:27 -0800

    updating macros

--- macros/get_ad_history_columns.sql
+++ macros/get_ad_history_columns.sql
@@ -1,18 +1,13 @@
 {% macro get_ad_history_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
     {"name": "ad_group_id", "datatype": dbt_utils.type_int()},
     {"name": "campaign_id", "datatype": dbt_utils.type_int()},
     {"name": "creation_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "creative_id", "datatype": dbt_utils.type_int()},
-    {"name": "creative_type", "datatype": dbt_utils.type_string()},
-    {"name": "deleted", "datatype": "boolean"},
     {"name": "id", "datatype": dbt_utils.type_int()},
     {"name": "modification_time", "datatype": dbt_utils.type_timestamp()},
     {"name": "name", "datatype": dbt_utils.type_string()},
     {"name": "org_id", "datatype": dbt_utils.type_int()},
-    {"name": "serving_status", "datatype": dbt_utils.type_string()},
     {"name": "status", "datatype": dbt_utils.type_string()}
 ] %}
 
