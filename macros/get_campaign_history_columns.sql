37ccd20018ef3a6c43242f9892c84b5b8aaaae22
Sheri Nguyen <sheri.nguyen@fivetran.com>
2022-09-21 12:58:01 -0800

    Updates for dbt-utils to dbt-core cross-db macro migration

--- macros/get_campaign_history_columns.sql
+++ macros/get_campaign_history_columns.sql
@@ -1,13 +1,13 @@
 {% macro get_campaign_history_columns() %}
 
 {% set columns = [
-    {"name": "end_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "id", "datatype": dbt_utils.type_int()},
-    {"name": "modification_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "name", "datatype": dbt_utils.type_string()},
-    {"name": "organization_id", "datatype": dbt_utils.type_int()},
-    {"name": "start_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "status", "datatype": dbt_utils.type_string()}
+    {"name": "end_time", "datatype": dbt.type_timestamp()},
+    {"name": "id", "datatype": dbt.type_int()},
+    {"name": "modification_time", "datatype": dbt.type_timestamp()},
+    {"name": "name", "datatype": dbt.type_string()},
+    {"name": "organization_id", "datatype": dbt.type_int()},
+    {"name": "start_time", "datatype": dbt.type_timestamp()},
+    {"name": "status", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
