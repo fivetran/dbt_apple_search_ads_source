84ccf22885116c70a75ab6f6e329812420c896dd
Sheri Nguyen <sheri.nguyen@fivetran.com>
2022-10-13 11:55:58 -0800

    Updates for dbt-utils to dbt-core cross-db macro migration

--- macros/get_ad_level_report_columns.sql
+++ macros/get_ad_level_report_columns.sql
@@ -1,16 +1,16 @@
 {% macro get_ad_level_report_columns() %}
 
 {% set columns = [
-    {"name": "ad_group_id", "datatype": dbt_utils.type_int()},
-    {"name": "ad_id", "datatype": dbt_utils.type_int()},
-    {"name": "campaign_id", "datatype": dbt_utils.type_int()},
+    {"name": "ad_group_id", "datatype": dbt.type_int()},
+    {"name": "ad_id", "datatype": dbt.type_int()},
+    {"name": "campaign_id", "datatype": dbt.type_int()},
     {"name": "date", "datatype": "date"},
-    {"name": "impressions", "datatype": dbt_utils.type_int()},
-    {"name": "local_spend_amount", "datatype": dbt_utils.type_numeric()},
-    {"name": "local_spend_currency", "datatype": dbt_utils.type_string()},
-    {"name": "new_downloads", "datatype": dbt_utils.type_int()},
-    {"name": "redownloads", "datatype": dbt_utils.type_int()},
-    {"name": "taps", "datatype": dbt_utils.type_int()}
+    {"name": "impressions", "datatype": dbt.type_int()},
+    {"name": "local_spend_amount", "datatype": dbt.type_numeric()},
+    {"name": "local_spend_currency", "datatype": dbt.type_string()},
+    {"name": "new_downloads", "datatype": dbt.type_int()},
+    {"name": "redownloads", "datatype": dbt.type_int()},
+    {"name": "taps", "datatype": dbt.type_int()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('apple_search_ads__ad_passthrough_metrics')) }}
--- macros/get_campaign_report_columns.sql
+++ macros/get_campaign_report_columns.sql
@@ -2,13 +2,13 @@
 
 {% set columns = [
     {"name": "date", "datatype": "date"},
-    {"name": "id", "datatype": dbt_utils.type_int()},
-    {"name": "impressions", "datatype": dbt_utils.type_int()},
-    {"name": "local_spend_amount", "datatype": dbt_utils.type_numeric()},
-    {"name": "local_spend_currency", "datatype": dbt_utils.type_string()},
-    {"name": "new_downloads", "datatype": dbt_utils.type_int()},
-    {"name": "redownloads", "datatype": dbt_utils.type_int()},
-    {"name": "taps", "datatype": dbt_utils.type_int()}
+    {"name": "id", "datatype": dbt.type_int()},
+    {"name": "impressions", "datatype": dbt.type_int()},
+    {"name": "local_spend_amount", "datatype": dbt.type_numeric()},
+    {"name": "local_spend_currency", "datatype": dbt.type_string()},
+    {"name": "new_downloads", "datatype": dbt.type_int()},
+    {"name": "redownloads", "datatype": dbt.type_int()},
+    {"name": "taps", "datatype": dbt.type_int()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('apple_search_ads__campaign_passthrough_metrics')) }}
