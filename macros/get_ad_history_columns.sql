c6c167c6e4cb3ec14e90eda142348ce990fc403c
Sheri Nguyen <sheri.nguyen@fivetran.com>
2022-10-13 15:20:15 -0800

    Updates for dbt-utils to dbt-core cross-db macro migration

--- macros/get_ad_history_columns.sql
+++ macros/get_ad_history_columns.sql
@@ -1,14 +1,14 @@
 {% macro get_ad_history_columns() %}
 
 {% set columns = [
-    {"name": "ad_group_id", "datatype": dbt_utils.type_int()},
-    {"name": "campaign_id", "datatype": dbt_utils.type_int()},
-    {"name": "creation_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "id", "datatype": dbt_utils.type_int()},
-    {"name": "modification_time", "datatype": dbt_utils.type_timestamp()},
-    {"name": "name", "datatype": dbt_utils.type_string()},
-    {"name": "org_id", "datatype": dbt_utils.type_int()},
-    {"name": "status", "datatype": dbt_utils.type_string()}
+    {"name": "ad_group_id", "datatype": dbt.type_int()},
+    {"name": "campaign_id", "datatype": dbt.type_int()},
+    {"name": "creation_time", "datatype": dbt.type_timestamp()},
+    {"name": "id", "datatype": dbt.type_int()},
+    {"name": "modification_time", "datatype": dbt.type_timestamp()},
+    {"name": "name", "datatype": dbt.type_string()},
+    {"name": "org_id", "datatype": dbt.type_int()},
+    {"name": "status", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
