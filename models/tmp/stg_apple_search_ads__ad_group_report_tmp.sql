bce675e9b097e331466b84aa4d709b86753be12e
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-09-05 21:44:23 -0500

    updates

--- models/tmp/stg_apple_search_ads__ad_group_report_tmp.sql
+++ models/tmp/stg_apple_search_ads__ad_group_report_tmp.sql
@@ -7,7 +7,7 @@
         schema_variable='apple_search_ads_schema', 
         default_database=target.database,
         default_schema='apple_search_ads',
-        default_variable='ad_group_report_source',
+        default_variable='ad_group_report',
         union_schema_variable='apple_search_ads_union_schemas',
         union_database_variable='apple_search_ads_union_databases'
     )
